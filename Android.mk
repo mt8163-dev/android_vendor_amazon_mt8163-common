aeeb0829e4b5019523e37fd3588d37754d0c5ca6
bilux <i.bilux@gmail.com>
2021-08-27 16:46:58 +0200

    mt8163-common: set up blueprint files

--- Android.mk
+++ Android.mk
@@ -18,15 +18,4 @@ LOCAL_PATH := $(call my-dir)
 
 ifneq ($(filter giza douglas,$(TARGET_DEVICE)),)
 
-include $(CLEAR_VARS)
-LOCAL_MODULE := libdpframework
-LOCAL_MODULE_OWNER := amazon
-LOCAL_SRC_FILES_64 := proprietary/lib64/libdpframework.so
-LOCAL_SRC_FILES_32 := proprietary/lib/libdpframework.so
-LOCAL_MULTILIB := both
-LOCAL_MODULE_TAGS := optional
-LOCAL_MODULE_CLASS := SHARED_LIBRARIES
-LOCAL_MODULE_SUFFIX := .so
-include $(BUILD_PREBUILT)
-
 endif
